fa368bfd44c5d5c4de3e808bb8252594638ec3a1
Utkarsh Kant <34377103+utkarshkant@users.noreply.github.com>
2025-08-22 17:22:34 +0530

    Update variables.dart
    Updated script

--- variables.dart
+++ variables.dart
@@ -2,20 +2,22 @@
 
 void main() {
   var name = 'mario';
-  print(name);
-  // name = 123; // data type of a `var` variable cannot be changed once declared
+  print(name);  // mario
+  // name = 123; // Error - data type of a `var` variable cannot be changed once declared
   name = 'luigi';
   print(name);
 
   final age = 25;
-  // age = 26;  // value of a `final` variable cannot be changed once declared
+  // age = 26;  // Error - value of a `final` variable cannot be changed once declared
   print(age);
 
   const isOpen = true;
   print(isOpen);
-  // isOpen = false; // value of a `const` variable cannot be changed once declared
+  // isOpen = false; // Error - value of a `const` variable cannot be changed once declared
 
-  // `final` vs `const`
-  // `final` is a runtime constant variable, `const` is a compile-time constant variable
-  // typically, we use final when we are unsure what the value will be at compile time
+  /* 
+  `final` vs `const`
+  - `final` is a runtime constant variable, `const` is a compile-time constant variable
+  - typically, we use final when we are unsure what the value will be at compile time
+  */
 }
