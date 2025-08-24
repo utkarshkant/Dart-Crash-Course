0320911b33d47b23e7cb7ece7a869755a49b6ebf
Utkarsh Kant <34377103+utkarshkant@users.noreply.github.com>
2025-08-24 11:45:05 +0530

    Update named_parameters.dart

--- named_parameters.dart
+++ named_parameters.dart
@@ -9,13 +9,13 @@ NAMED PARAMETERS (POSITION ARGUMENTS)
 // `main` function
 void main() {
 
-  final namasteGreeting = namaste(personName: "Mohan", personAge: 14);
-  print(namasteGreeting); // Hi, my name is Mohan and I am 14 years old.
+  final intro1 = introduce(personName: "Mohan", personAge: 14);
+  print(intro1); // Hi, my name is Mohan and I am 14 years old.
 
-  final namasteGreeting2 = namaste(personAge: 15);
-  print(namasteGreeting2); // Hi, my name is null and I am 15 years old.
+  final intro2 = introduce(personAge: 15);
+  print(intro2); // Hi, my name is null and I am 15 years old.
 }
 
-String namaste({String? personName, required int personAge}) {
+String introduce({String? personName, required int personAge}) {
   return "Hi, my name is $personName and I am $personAge years old.";
 }
