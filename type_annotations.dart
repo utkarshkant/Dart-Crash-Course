d00a9f67bff977b293f06e49a470ad3af935e4cf
Utkarsh Kant <34377103+utkarshkant@users.noreply.github.com>
2025-08-24 11:40:43 +0530

    Update type_annotations.dart

--- type_annotations.dart
+++ type_annotations.dart
@@ -8,8 +8,7 @@ void main() {
 
   int age = 25;
   print(age); // 25
-  age =
-      34; // value of a type declared variable can be changed, but the type cannot be changed once declared
+  age = 34; // value of a type declared variable can be changed, but the type cannot be changed once declared
   print(age); // 34
 
   bool isOpen = true;
@@ -17,17 +16,16 @@ void main() {
 
   double avgRating = 8.9;
   print(avgRating); // 8.9
-  avgRating =
-      9; // value can be `int` in a `double` type variable, but the opposite is not true
+  avgRating = 9; // value can be `int` in a `double` type variable, but the opposite is not true
   print(avgRating); // 9
 
-  // Type annotated variables behave like `var` variables. However, if we want them to behave as `const` or `final`, then we can simply declare that.
+  //Type-annotated variables behave like `var` variables. However, if we want them to behave as `const` or `final`, then we can simply declare that.
 
   const String surname = "James";
   print("My name is $name $surname."); // My name is mario James.
   // surname = "Page"; // Error: Cannot assign a new value to a `const` or `final` variable.
   
-  // Mostly we use `var` to declare the variables as Dart detects the correct datatype, unless we want to be very specific. Like `double`, etc.
+  // Mostly, we use `var` to declare variables, as Dart detects the correct datatype, unless we want to be very specific. Like `double`, etc.
   
   
 }
